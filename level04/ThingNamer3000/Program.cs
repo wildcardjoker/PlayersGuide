307a5732196bfc3df141cca6155110ffe330948f
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-01-10 17:58:48 +0800

    Fix typo.

--- level04/ThingNamer3000/Program.cs
+++ level04/ThingNamer3000/Program.cs
@@ -22,17 +22,17 @@
  * Answer this question: Aside from comments, what else could you do to make this code more understandable?
  */
 
-
 // Ugh ... this feels so wrong...
 
 Console.WriteLine("What kind of thing are we talking about?");
+
 // a = The kind of thing
 var a = Console.ReadLine();
 Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
 /* b = The thing's description */
 var b = Console.ReadLine();
 var c = "of Doom"; // Kind of obvious. Besides, this should be a constant
-var d = "3000"; // Also obvious. Again, should be a constant. Or even better, an assembly version, or a Setting, or some other easily updateable property.
+var d = "3000";    // Also obvious. Again, should be a constant. Or even better, an assembly version, or a Setting, or some other easily updateable property.
 
 // Anyway, on to the bug fixing
 // The original line includes the string " of ", then appends "of Doom", making the output "The {b} {a} of of Doom!"
@@ -50,7 +50,7 @@ Console.WriteLine("The " + b + " " + a + " " + c + " " + d + "!");
  *
  * Secondly, let's make c and d constants (OK, the book hasn't gotten there yet. I get it)
  *
- * Third, replace that godawful concatention with string interpolation. Again, the book hasn't raised this issue,
+ * Third, replace that godawful concatenation with string interpolation. Again, the book hasn't raised this issue,
  * and we've just gone through a chapter about writing comments, so I'll let it slide. But that line is just begging to be
  * written as
  * Console.WriteLine($"The {b} {a} {c} {d}!");
