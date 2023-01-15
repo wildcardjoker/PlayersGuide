92f2fdaf3fea62d6c3bc5831e2312f3a734d7195
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-01-15 10:25:07 +0800

    Add documentation for Discounted Inventory.

--- level10/DiscountedInventory/Program.cs
+++ level10/DiscountedInventory/Program.cs
@@ -1,20 +1,4 @@
-﻿/*
- * Discounted Inventory
- *
- * After sorting through Tortuga's outfitter shop and making it viable again, Tortuga realises you've put him back in business. he
- * wants to repay the favour by giving you a 50% discount on anything you buy from him, and he wants you to modify your program to
- * reflect that.
- *
- * After asking the user for a number, the program should also ask for their name. If the name supplied is your name, cut the price
- * in half before reporting it to the user.
- *
- * Objectives:
- *
- * Modify the BuyingInventory program to also ask the user for their name
- * If their name equals your name, divide the cost in half.
- */
-
-// Let's create variables for the prices
+﻿// Let's create variables for the prices
 // While the actual value of the halved costs isn't related to the sample, we'll make the costs a decimal value for the sake of
 // completeness
 
---
+++ level10/DiscountedInventory/Readme.md
@@ -0,0 +1,10 @@
+# Discounted Inventory
+
+After sorting through Tortuga's outfitter shop and making it viable again, Tortuga realises you've put him back in business. He wants to repay the favour by giving you a 50% discount on anything you buy from him, and he wants you to modify your program to reflect that.
+
+After asking the user for a number, the program should also ask for their name. If the name supplied is your name, cut the price in half before reporting it to the user.
+
+## Objectives
+
+- Modify the `BuyingInventory` program to also ask the user for their name.
+- If their name equals your name, divide the cost in half.
