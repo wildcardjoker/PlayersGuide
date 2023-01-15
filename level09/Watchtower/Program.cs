6c4b79784b49a1eb3d800d2d77bb5a4d43a99b7d
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-01-15 10:18:25 +0800

    Add documentation for Watchtower.

--- level09/Watchtower/Program.cs
+++ level09/Watchtower/Program.cs
@@ -1,23 +1,4 @@
-﻿/*
- * Watchtower
- *
- * There are watchtowers in teh region around Consolas that can alert you when an enemy is spotted. With some help from you, they can
- * tell you which direction the enemy is from the watchtower.
- *
- * Objectives:
- *
- * Ask the user for an x value and a y value. These are coordinates of the enemy relative to the watchtower's location.
- * Using the map below, `if` statements, and relational operators, display a message about what direction the enemy is coming from.
- * For example, "The enemy is to the northwest!" or "The enemy is here!"
- *
- *     x<0 x=0 x>0
- * y>0 NW   N   NE
- * y=0  W   !    E
- * y<0 SW   S   SE
- *
- */
-
-Console.Title = "The Watchtower";
+﻿Console.Title = "The Watchtower";
 Console.WriteLine("Please enter the coordinates of the enemy.");
 Console.WriteLine();
 Console.Write("Please enter the x coordinates: ");
---
+++ level09/Watchtower/Readme.md
@@ -0,0 +1,14 @@
+# Watchtower
+
+There are watchtowers in teh region around Consolas that can alert you when an enemy is spotted. With some help from you, they can tell you which direction the enemy is from the watchtower.
+
+## Objectives
+
+- Ask the user for an `x` value and a `y` value. These are coordinates of the enemy relative to the watchtower's location.
+- Using the map below, `if` statements, and relational operators, display a message about what direction the enemy is coming from.
+- For example, "`The enemy is to the northwest!`" or "`The enemy is here!`"
+
+            x<0 x=0 x>0
+        y>0 NW   N   NE
+        y=0  W   !    E
+        y<0 SW   S   SE
