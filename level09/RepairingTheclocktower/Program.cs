bf284e44ef18f2adc46062146f08f84c9baeca9a
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-01-15 10:15:04 +0800

    Add documentation for Repairing The Clocktower.

--- level09/RepairingTheclocktower/Program.cs
+++ level09/RepairingTheclocktower/Program.cs
@@ -1,18 +1,4 @@
-﻿/*
- * Repairing the Clocktower
- *
- * The recent attacks damaged the great Clocktower of Consolas. The citizens of Consolas have repaired most of it, except one piece
- * that requires the steady hand of a Programmer. It is the part that makes the clock tick and tock. Numbers flow into the clock to
- * make it go, and if the number is even, the clock's pendulum should tick to to the left; if the number is odd, the pendulum should
- * tock to the right. Only a programmer can recreate this critical clock element to make it work again.
- *
- * Objectives:
- * Take a number as input from the console
- * Display the word "Tick" if the number is even. Display the word "Tock" if the number is odd.
- * Hint: Remember that you can use the remainder operator to determine if a number is even or odd.
- */
-
-Console.Title = "Repairing the Clocktower";
+﻿Console.Title = "Repairing the Clocktower";
 Console.WriteLine("Numbers flow into the clock");
 Console.WriteLine("The clock will -tick- if the number is even, and -tock- if the number is odd.");
 Console.Write("\nWhat is the number? ");
