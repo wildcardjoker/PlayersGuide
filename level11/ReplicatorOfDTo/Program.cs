a1c28b4dea0abcd9c37825d7172a93d87b6c858b
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-01-15 10:54:52 +0800

    Add documentation for The Replicator of D'To.

--- level11/ReplicatorOfDTo/Program.cs
+++ level11/ReplicatorOfDTo/Program.cs
@@ -1,19 +1,4 @@
-﻿/*
- * The Replicator of D'To
- *
- * While searching an abandoned storage building containing strange code artifacts, you uncover the ancient Replicator of D'To. This
- * can replicate the contents of any int array into another array. But it appears broken and needs a Programmer to reforge the magic
- * that allows it to replicate once again.
- *
- * Objectives:
- * Make a program that creates an array of length 5.
- * Ask the user for five numbers and put them in the array.
- * Make a second array of length 5.
- * Use a loop to copy the values out of the original array and into the new one.
- * Display the contents of both arrays one at a time to illustrate that the Replicator of D/To works again.
- */
-
-// While there are easier ways to copy an array, we'll do it the 'hard' way.
+﻿// While there are easier ways to copy an array, we'll do it the 'hard' way.
 
 /* For reference, here are some ways to reference the values in an array
  array[i]       Reference the value at index i
---
+++ level11/ReplicatorOfDTo/Readme.md
@@ -0,0 +1,11 @@
+# The Replicator of D'To
+
+While searching an abandoned storage building containing strange code artifacts, you uncover the ancient `Replicator of D'To`. This can replicate the contents of any `int array` into another `array`. But it appears broken and needs a Programmer to reforge the magic that allows it to replicate once again.
+
+## Objectives
+
+- Make a program that creates an `array` of length 5.
+- Ask the user for five numbers and put them in the `array`.
+- Make a second `array` of length 5.
+- Use a loop to copy the values out of the original `array` and into the new one.
+- Display the contents of both `arrays` one at a time to illustrate that the `Replicator of D'To` works again.
