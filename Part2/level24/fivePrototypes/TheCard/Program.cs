db2d8b44307364482771afaceca5ba770f4b51ec
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-01-29 10:43:40 +0800

    Add The Card prototype.

---
+++ Part2/level24/fivePrototypes/TheCard/Program.cs
@@ -0,0 +1,64 @@
+﻿// Ideally, since we're dealing with enums, we'd use the Enum. methods to iterate through all possible values.
+// Since that hasn't been covered yet, we'll use magic numbers to convert the counter to the required enum.
+// RB's example used an array for Colour and Rank. Rather than update my code, I've decided to leave it as originally created. This gives users two viable examples.
+
+for (var i = 0; i < 4; i++)
+{
+    var colour = (Colour) i;
+    for (var j = 0; j < 14; j++)
+    {
+        var rank = (Rank) j;
+        var card = new Card(colour, rank);
+        Console.WriteLine($"{card.CardDescription}: {card.CardValueType}");
+    }
+}
+
+// Answer this question: Why do you think we used a colour enumeration here but make a Colour class in the previous challenge?
+// Teh colours are static, and will never change. The application as currently designed will not be required to create variations on these colours.
+
+public class Card
+{
+    #region Constructors
+    public Card(Colour colour, Rank rank)
+    {
+        CardColour = colour;
+        CardRank   = rank;
+    }
+    #endregion
+
+    #region Properties
+    public Colour CardColour {get;}
+
+    public string CardDescription => $"The {CardColour} {CardRank}";
+    public Rank   CardRank        {get;}
+    public string CardValueType   => IsNumberCard ? "Number card" : "Symbol Card";
+    public bool   IsNumberCard    => (int) CardRank <= 9;
+    public bool   IsSymbolCard    => (int) CardRank > 10;
+    #endregion
+}
+
+public enum Colour
+{
+    Blue,
+    Green,
+    Red,
+    Yellow
+}
+
+public enum Rank
+{
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Ten,
+    Currency,
+    Percent,
+    Caret,
+    Ampersand
+}
---
+++ Part2/level24/fivePrototypes/TheCard/README.md
@@ -0,0 +1,15 @@
+# The Card
+
+The digital Realms of C# have playing cards like ours, but with some differences.
+
+Each card has a colour (`red`, `green`, `blue`, `yellow`) and a rank (the number `1` through `10`, followed by the symbols `$`, `%`, `^`, and `&`).
+
+The third pedestal requires that you create a class to represent a card of this nature.
+
+## Objectives
+
+- Define enumerations for card `colours` and card `ranks`.
+- Define a `Card` class to represent a card with a `colour` and a `rank`, as described above.
+- Add properties or methods that tell you if a card is a number of symbol card (the equivalent of a face card).
+- Create a main method that will create a card instance for the whole deck (every colour with every rank) and display each (for example, "The Red Ampersand", and "The Blue Seven").
+- **Answer this question**: Why do you think we used a colour enumeration here but make a Colour class in the previous challenge?
