ebb336af04b7b7e58fdccae58e9637cfeedf3065
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-02-09 17:45:44 +0800

    Add InventoryItem class.

--- Part2/level25/packingInventory/Program.cs
+++ Part2/level25/packingInventory/Program.cs
@@ -1,9 +1,25 @@
-﻿// Pack class
-// InventoryItem class
+﻿// InventoryItem class
+
+public class InventoryItem
+{
+    #region Constructors
+    public InventoryItem(float weight, float volume)
+    {
+        Weight = weight;
+        Volume = volume;
+    }
+    #endregion
+
+    #region Properties
+    public float Volume {get;}
+    public float Weight {get;}
+    #endregion
+}
+
 // Arrow class
 // Bow class
 // Rope class
 // Water class
 // FoodRation class
 // Sword class
-
+// // Pack class
