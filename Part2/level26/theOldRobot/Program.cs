7c0f0d81da353699e69a4e6418f0a3f9cf0a3778
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-02-12 09:56:06 +0800

    Add theOldRobot project.

---
+++ Part2/level26/theOldRobot/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Robot
+{
+    #region Properties
+    public RobotCommand?[] Commands  {get;} = new RobotCommand?[3];
+    public bool            IsPowered {get; set;}
+    public int             X         {get; set;}
+    public int             Y         {get; set;}
+    #endregion
+
+    public void Run()
+    {
+        foreach (var command in Commands)
+        {
+            command?.Run(this);
+            Console.WriteLine($"[{X} {Y} {IsPowered}]");
+        }
+    }
+}
+
+public abstract class RobotCommand
+{
+    public abstract void Run(Robot robot);
+}
