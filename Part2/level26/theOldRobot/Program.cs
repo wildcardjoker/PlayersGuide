a7f373c744dc70a2711b85f5463d5a2f7c44da0f
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-02-12 10:13:43 +0800

    Allow the user to program the robot, and display the output.

--- Part2/level26/theOldRobot/Program.cs
+++ Part2/level26/theOldRobot/Program.cs
@@ -1,4 +1,54 @@
-﻿public class Robot
+﻿Console.Title = "The Old Robot";
+Console.WriteLine(Console.Title);
+
+// Create a set of valid commands
+var validCommands = new[] {"on", "off", "north", "south", "east", "west"};
+
+// Set the number of commands supported by the robot
+var numCommands = 3;
+
+// Create a new robot
+var robot = new Robot();
+
+DisplayValidCommands();
+
+// Enter the commands for the robot, as determined by the user
+for (var i = 0; i < numCommands; i++)
+{
+    robot.Commands[i] = GetCommand();
+}
+
+Console.WriteLine();
+
+// Initiate the Robot's command list.
+robot.Run();
+
+RobotCommand GetCommand()
+{
+    // Assume that the user has entered a valid command.
+    // We should include validation and error handling here, but it's just an exercise.
+    Console.Write("Please enter a command: ");
+    return Console.ReadLine() switch
+    {
+        "on"    => new OnCommand(),
+        "off"   => new OffCommand(),
+        "north" => new NorthCommand(),
+        "south" => new SouthCommand(),
+        "east"  => new EastCommand(),
+        "west"  => new WestCommand()
+    };
+}
+
+void DisplayValidCommands()
+{
+    Console.WriteLine("\n\nValid robot commands are:");
+    foreach (var command in validCommands)
+    {
+        Console.WriteLine($"\t{command}");
+    }
+}
+
+public class Robot
 {
     #region Properties
     public RobotCommand?[] Commands  {get;} = new RobotCommand?[3];
