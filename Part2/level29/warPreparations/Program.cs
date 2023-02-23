a07eb769f6a1bddc08e85d966a0fbadff82f42c9
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-02-23 20:22:54 +0800

    Add enum for Material and Gemstone.

---
+++ Part2/level29/warPreparations/Program.cs
@@ -0,0 +1,18 @@
+﻿internal enum Material
+{
+    Binarium,
+    Bronze,
+    Iron,
+    Steel,
+    Wood
+}
+
+internal enum Gemstone
+{
+    None,
+    Amber,
+    Bitstone,
+    Diamond,
+    Emerald,
+    Sapphire
+}
