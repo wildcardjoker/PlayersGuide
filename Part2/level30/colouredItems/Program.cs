66f3e499566edd177cb08c573b118b28c0c08c6a
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-02-23 21:03:17 +0800

    Add ColouredItem<T> class.

--- Part2/level30/colouredItems/Program.cs
+++ Part2/level30/colouredItems/Program.cs
@@ -3,3 +3,22 @@
 public class Bow {}
 
 public class Axe {}
+
+/// <summary>
+///     Assign a colour to any item type.
+/// </summary>
+/// <typeparam name="T">The item type to be coloured.</typeparam>
+public class ColouredItem<T>
+{
+    #region Properties
+    private ConsoleColor Color {get; init;}
+    #endregion
+
+    /// <summary>
+    ///     Change the console colour to the object's specified colour.
+    /// </summary>
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+    }
+}
