0becfa16840d881ec2eb666b7d2a79f762e84808
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-02-23 20:29:33 +0800

    Add three swords. Two swords are modified from the original object.

--- Part2/level29/warPreparations/Program.cs
+++ Part2/level29/warPreparations/Program.cs
@@ -1,4 +1,14 @@
-﻿internal enum Material
+﻿var original       = new Sword(SourceMaterial.Iron, AugmentationGemstone.None, 10, 4);
+var augmentedSword = original with {Gemstone = AugmentationGemstone.Diamond};
+var superSword     = original with {Gemstone = AugmentationGemstone.Bitstone, Length = 15, Material = SourceMaterial.Binarium};
+
+Console.WriteLine(original);
+Console.WriteLine(augmentedSword);
+Console.WriteLine(superSword);
+
+internal record Sword(SourceMaterial Material, AugmentationGemstone Gemstone, int Length, int Crossguard);
+
+internal enum SourceMaterial
 {
     Binarium,
     Bronze,
@@ -7,7 +17,7 @@
     Wood
 }
 
-internal enum Gemstone
+internal enum AugmentationGemstone
 {
     None,
     Amber,
