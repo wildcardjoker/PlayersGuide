e6bab3f776ac6ac7d174a0bc28912badba65a7f4
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-03-05 17:00:16 +0800

    Add ColouredItem class from ColouredItems (Level 30).

--- Part2/level31/fountainOfObjects/Program.cs
+++ Part2/level31/fountainOfObjects/Program.cs
@@ -6,13 +6,14 @@ var rooms = new[,] {{0, 1, 2}, {0, 1, 2}};
 var fountainLocation = new Point(0, 2);
 var entranceLocation = new Point(0, 0);
 var currentLocation  = entranceLocation;
+var narrativeItem    = new ColouredItem<string>($"You are in the room at {currentLocation}", ConsoleColor.Magenta);
 
 // Simulate the player moving to the room
-Console.WriteLine(currentLocation);
+narrativeItem.Display();
 Move(Direction.East);
-Console.WriteLine(currentLocation);
+narrativeItem.Display();
 Move(Direction.East);
-Console.WriteLine(currentLocation);
+narrativeItem.Display();
 
 // Check that we are at the Fountain's location.
 Console.WriteLine(currentLocation == fountainLocation);
@@ -46,7 +47,7 @@ internal record Point(int Row, int Column)
 {
     #region Overrides of Object
     /// <inheritdoc />
-    public override string ToString() => $"You are in the room at (Row={Row}, Column={Column})";
+    public override string ToString() => $"(Row={Row}, Column={Column})";
     #endregion
 }
 
@@ -58,4 +59,39 @@ internal enum Direction
     East,
     West,
     Unknown
+}
+
+/// <summary>
+///     Assign a colour to any item type.
+/// </summary>
+/// <typeparam name="T">The item type to be coloured.</typeparam>
+public class ColouredItem<T>
+{
+    #region Constructors
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="ColouredItem{T}" /> class.
+    /// </summary>
+    /// <param name="item">The item to be colourised.</param>
+    /// <param name="color">The color.</param>
+    public ColouredItem(T item, ConsoleColor color)
+    {
+        Item       = item;
+        ItemColour = color;
+    }
+    #endregion
+
+    #region Properties
+    private T            Item       {get;}
+    private ConsoleColor ItemColour {get;}
+    #endregion
+
+    /// <summary>
+    ///     Change the console colour to the object's specified colour.
+    /// </summary>
+    public void Display()
+    {
+        Console.ForegroundColor = ItemColour;
+        Console.WriteLine(Item?.ToString());
+        Console.ResetColor();
+    }
 }
