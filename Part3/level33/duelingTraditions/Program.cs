fd1e7e352d395ec7a87b1c28268971ffb17e7792
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-04-09 11:45:36 +0800

    Move Point() to separate file.

--- Part3/level33/duelingTraditions/Program.cs
+++ Part3/level33/duelingTraditions/Program.cs
@@ -423,13 +423,6 @@ internal static class DuelingTraditions
 
     #region Internal declarations
     // Create a simple object to hold the player's location
-    internal record Point(int Row, int Column)
-    {
-        #region Overrides of Object
-        /// <inheritdoc />
-        public override string ToString() => $"(Row={Row}, Column={Column})";
-        #endregion
-    }
 
     // Compass Directions
     internal enum Direction
