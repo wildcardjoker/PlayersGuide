36e96bfe8a0b604e0f08f3f110a011d571758e86
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-04-09 11:13:11 +0800

    Add IField.Pig and McDroid.Pig.
    Use alias and qualified reference.

--- Part3/level33/theFeud/Program.cs
+++ Part3/level33/theFeud/Program.cs
@@ -3,7 +3,15 @@
 #region Using Directives
 using theFeud.IField;
 using theFeud.McDroid;
+using Pig = theFeud.IField.Pig;
 #endregion
 
-var sheep = new Sheep();
-var cow   = new Cow();
+var sheep      = new Sheep();
+var cow        = new Cow();
+var iFieldPig  = new Pig();                 // uses alias
+var mcDroidPig = new theFeud.McDroid.Pig(); // Qualified reference
+
+sheep.Speak();
+cow.Speak();
+iFieldPig.Speak();
+mcDroidPig.Speak();
