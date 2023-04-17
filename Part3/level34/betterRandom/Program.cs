98147ec87f15d190d201ea364f74a33ce71050e6
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-04-17 14:30:28 +0800

    Call the random Extension methods using default parameters.

--- Part3/level34/betterRandom/Program.cs
+++ Part3/level34/betterRandom/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#region Using Directives
+using betterRandom;
+#endregion
+
+var strings = new[] {"up", "down", "left", "right"};
+var random  = new Random();
+
+Console.WriteLine($"Next Double: {random.NextDouble()}");
+Console.WriteLine($"Next String: {random.NextString(strings)}");
+Console.WriteLine($"Coin Flip  : {(random.CoinFlip() ? "Heads" : "Tails")}");
