abcdd74ddf285db42e3210d60e210858c809a307
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-04-17 15:13:40 +0800

    Allow user to specify number of coin flips and frequency of Heads. Show average of results.

--- Part3/level34/betterRandom/Program.cs
+++ Part3/level34/betterRandom/Program.cs
@@ -7,32 +7,7 @@ var random  = new Random();
 
 ProcessDoubles();
 ProcessStrings();
-
-Console.WriteLine($"Coin Flip  : {(random.CoinFlip() ? "Heads" : "Tails")}");
-
-#region Double
-void ProcessDoubles()
-{
-    var numberOfDoubles = GetNumberOfRandomObjects("double");
-    var doubleMaximum   = GetDoubleMaximum();
-    WriteDoubles(numberOfDoubles, doubleMaximum);
-}
-
-void WriteDoubles(int cycles, double maximum)
-{
-    for (var i = 0; i < cycles; i++)
-    {
-        Console.WriteLine($"Next Double: {random.NextDouble(maximum)}");
-    }
-}
-
-double GetDoubleMaximum()
-{
-    Console.Write("What is the maximum for your Double values? ");
-    double.TryParse(Console.ReadLine(), out var result);
-    return result;
-}
-#endregion
+ProcessCoinFlips();
 
 int GetNumberOfRandomObjects(string objectDescription, int defaultNumber = 10)
 {
@@ -45,6 +20,31 @@ int GetNumberOfRandomObjects(string objectDescription, int defaultNumber = 10)
     return result;
 }
 
+#region Double
+void ProcessDoubles()
+{
+    var numberOfDoubles = GetNumberOfRandomObjects("double");
+    var doubleMaximum   = GetDoubleMaximum("What is the maximum for your Double values?");
+    WriteDoubles(numberOfDoubles, doubleMaximum);
+}
+
+void WriteDoubles(int cycles, double maximum)
+{
+    maximum = maximum == 0 ? 1 : maximum;
+    for (var i = 0; i < cycles; i++)
+    {
+        Console.WriteLine($"Next Double: {random.NextDouble(maximum)}");
+    }
+}
+
+double GetDoubleMaximum(string s)
+{
+    Console.Write($"{s} ");
+    double.TryParse(Console.ReadLine(), out var result);
+    return result;
+}
+#endregion
+
 #region Strings
 void ProcessStrings() => WriteStrings(GetNumberOfRandomObjects("string"));
 
@@ -55,4 +55,41 @@ void WriteStrings(int cycles)
         Console.WriteLine($"Next string: {random.NextString(strings)}");
     }
 }
+#endregion
+
+#region Coin Flip
+void ProcessCoinFlips()
+{
+    var numberOfFlips  = GetNumberOfRandomObjects("coin flip", 100);
+    var headsFrequency = GetDoubleMaximum("How often should Heads show? Default is 0.5 (50%):");
+    Console.Write("Show individual results [Y/N]? ");
+    var showIndividualResults = Console.ReadLine()?.ToUpper().Equals("Y") ?? false;
+
+    var headCount = 0;
+    var tailCount = 0;
+    for (var i = 0; i < numberOfFlips; i++)
+    {
+        var coinFlipResult = random.CoinFlip(headsFrequency == 0 ? 0.5 : headsFrequency);
+        if (showIndividualResults)
+        {
+            Console.WriteLine($"Coin Flip  : {(coinFlipResult ? "Heads" : "Tails")}");
+        }
+
+        if (coinFlipResult)
+        {
+            headCount++;
+        }
+        else
+        {
+            tailCount++;
+        }
+    }
+
+    Console.WriteLine("\nResults:");
+
+    Console.WriteLine($"Heads: {headCount} ({GetPercent(headCount, numberOfFlips)} %)");
+    Console.WriteLine($"Tails: {tailCount} ({GetPercent(tailCount, numberOfFlips)} %)");
+}
+
+double GetPercent(int count, int max) => Math.Round((double) count / max * 100, 2);
 #endregion
