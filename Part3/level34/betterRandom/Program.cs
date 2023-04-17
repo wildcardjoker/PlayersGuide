360ac6e52c00069cdfb51ff3c5d40577b324d6ed
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-04-17 14:41:56 +0800

    Add methods to allow user to specify number and maximum value of Double randoms.

--- Part3/level34/betterRandom/Program.cs
+++ Part3/level34/betterRandom/Program.cs
@@ -5,6 +5,34 @@ using betterRandom;
 var strings = new[] {"up", "down", "left", "right"};
 var random  = new Random();
 
-Console.WriteLine($"Next Double: {random.NextDouble()}");
+var numberOfDoubles = GetNumberOfRandomDoubles();
+var doubleMaximum   = GetDoubleMaximum();
+WriteDoubles(numberOfDoubles, doubleMaximum);
 Console.WriteLine($"Next String: {random.NextString(strings)}");
 Console.WriteLine($"Coin Flip  : {(random.CoinFlip() ? "Heads" : "Tails")}");
+
+void WriteDoubles(int cycles, double maximum)
+{
+    for (var i = 0; i < cycles; i++)
+    {
+        Console.WriteLine($"Next Double: {random.NextDouble(maximum)}");
+    }
+}
+
+int GetNumberOfRandomDoubles()
+{
+    Console.Write("How many doubles (default: 10)? ");
+    if (!int.TryParse(Console.ReadLine(), out var result))
+    {
+        result = 10;
+    }
+
+    return result;
+}
+
+double GetDoubleMaximum()
+{
+    Console.Write("What is the maximum for your Double values? ");
+    double.TryParse(Console.ReadLine(), out var result);
+    return result;
+}
