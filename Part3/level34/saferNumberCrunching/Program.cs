ea4bb5b7721252e9e45851cf5e9530b0176cc6ca
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-04-17 09:55:26 +0800

    Add Safer Number Crunching - use int.TryParse() to convert string to int.

---
+++ Part3/level34/saferNumberCrunching/Program.cs
@@ -0,0 +1,12 @@
+﻿string? s;
+int? value=null;
+while (value==null)
+{
+    Console.Write("Please enter a number: ");
+    s = Console.ReadLine();
+    if (int.TryParse(s,out int result))
+    {
+        value = result;
+    }
+}
+Console.WriteLine(value);
