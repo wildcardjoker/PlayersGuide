c931669e9f21f10a76f029a9a94cee90b17a945a
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-04-21 14:37:07 +0800

    Add 2-player support.

--- Part3/level35/exceptisGame/Program.cs
+++ Part3/level35/exceptisGame/Program.cs
@@ -5,18 +5,24 @@ var random          = new Random();
 var oatmealCookie   = random.Next(0, 10); // Choose between 0 - 9
 var previousChoices = new List<int>();
 var playerNumber    = 1;
+var cookieChoice    = -1;
 
-// Get player 1's cookie choice
-// Check against oatmeal cookie
+while (cookieChoice != oatmealCookie)
+{
+    cookieChoice = GetCookieChoice();
+    SwitchPlayer();
+}
 
-var cookieChoice = GetCookieChoice();
+// throw exception
+Console.WriteLine("Oatmeal cookie found!");
+Console.WriteLine($"Player {playerNumber} wins!");
 
 int GetCookieChoice()
 {
     var choice = -1;
     while (choice < 0 || choice > 9)
     {
-        Console.Write("Choose a cookie between 0 and 9: ");
+        Console.Write($"Player {playerNumber}, please choose a cookie between 0 and 9: ");
         if (!int.TryParse(Console.ReadLine(), out choice))
         {
             choice = -1;
@@ -24,4 +30,9 @@ int GetCookieChoice()
     }
 
     return choice;
+}
+
+void SwitchPlayer()
+{
+    playerNumber = playerNumber == 1 ? 2 : 1;
 }
