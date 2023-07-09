65c16505a894b5b069eeb42835f028a718f6f570
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-07-09 15:51:23 +0800

    Add Harvester.

--- Part3/level37/charberryTree/Program.cs
+++ Part3/level37/charberryTree/Program.cs
@@ -1,9 +1,13 @@
 ﻿#region Using Directives
 using charberryTree;
+
+// ReSharper disable UnusedVariable
+#pragma warning disable IDE0059
 #endregion
 
-var tree     = new CharberryTree();
-var notifier = new Notifier(tree);
+var tree      = new CharberryTree();
+var notifier  = new Notifier(tree);
+var harvester = new Harvester(tree);
 
 while (true)
 {
