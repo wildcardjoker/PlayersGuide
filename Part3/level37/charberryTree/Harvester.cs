cddddffa26017e7d4491ef9de15e01bfc3a0d1db
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-07-09 15:50:49 +0800

    Add Harvester class.
    Subscribe to Ripened event and reset Ripe property after harvesting.

---
+++ Part3/level37/charberryTree/Harvester.cs
@@ -0,0 +1,23 @@
+﻿namespace charberryTree
+{
+    internal class Harvester
+    {
+        #region Fields
+        private readonly CharberryTree _tree;
+        #endregion
+
+        #region Constructors
+        public Harvester(CharberryTree tree)
+        {
+            _tree         =  tree;
+            _tree.Ripened += Tree_Ripened;
+        }
+        #endregion
+
+        private void Tree_Ripened()
+        {
+            Console.WriteLine("The charberry fruit has been harvested.");
+            _tree.Ripe = false;
+        }
+    }
+}
