5a0663ab5024b64e5b29f4e4ff9913aebca4e72b
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-07-09 15:33:26 +0800

    Add initial charberryTree challenge.

---
+++ Part3/level37/charberryTree/CharberryTree.cs
@@ -0,0 +1,22 @@
+﻿namespace charberryTree
+{
+    public class CharberryTree
+    {
+        #region Fields
+        private readonly Random _random = new ();
+        #endregion
+
+        #region Properties
+        public bool Ripe {get; set;}
+        #endregion
+
+        public void MaybeGrow()
+        {
+            // Only a tiny chance of ripening each time, but we try a lot!
+            if (_random.NextDouble() < 0.00000001 && !Ripe)
+            {
+                Ripe = true;
+            }
+        }
+    }
+}
---
+++ Part3/level37/charberryTree/Program.cs
@@ -0,0 +1,10 @@
+﻿#region Using Directives
+using charberryTree;
+#endregion
+
+var tree = new CharberryTree();
+
+while (true)
+{
+    tree.MaybeGrow();
+}
