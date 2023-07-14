c731102145d94d6dc2f784b98615e45d2f94b30e
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-07-14 14:03:54 +0800

    Loop until user decides to complete potion.
    Accept number row or number pad.

--- Part3/level40/potionMastersofPattren/Program.cs
+++ Part3/level40/potionMastersofPattren/Program.cs
@@ -14,19 +14,20 @@ foreach (var ingredient in Enum.GetValues(typeof(Ingredient)))
 Console.WriteLine("C: Complete the potion as-is\n");
 Console.Write("Please enter your choice: ");
 var choice = Console.ReadKey().Key;
-if (choice == ConsoleKey.C)
+while (choice != ConsoleKey.C)
 {
-    Console.WriteLine($"\n\nYou are satisfied with the results of your alchemy, and label your vial: \"{potion.Humanize()}\"");
+    var nextIngredient = choice switch
+    {
+        ConsoleKey.D1 or ConsoleKey.NumPad1 => Ingredient.Stardust,
+        ConsoleKey.D2 or ConsoleKey.NumPad2 => Ingredient.SnakeVenom,
+        ConsoleKey.D3 or ConsoleKey.NumPad3 => Ingredient.DragonBreath,
+        ConsoleKey.D4 or ConsoleKey.NumPad4 => Ingredient.ShadowGlass,
+        ConsoleKey.D5 or ConsoleKey.NumPad5 => Ingredient.EyeShine,
+        _                                   => Ingredient.Water
+    };
+
+    Console.WriteLine($"\nYou add {nextIngredient.Humanize()} to your vial of {potion.Humanize()}.");
+    choice = Console.ReadKey().Key;
 }
 
-var nextIngredient = choice switch
-{
-    ConsoleKey.D1 => Ingredient.Stardust,
-    ConsoleKey.D2 => Ingredient.SnakeVenom,
-    ConsoleKey.D3 => Ingredient.DragonBreath,
-    ConsoleKey.D4 => Ingredient.ShadowGlass,
-    ConsoleKey.D5 => Ingredient.EyeShine,
-    _             => Ingredient.Water
-};
-
-Console.WriteLine($"\nYou add {nextIngredient.Humanize()} to your vial of {potion.Humanize()}.");
+Console.WriteLine($"\n\nYou are satisfied with the results of your alchemy, and label your vial: \"{potion.Humanize()}\"");
