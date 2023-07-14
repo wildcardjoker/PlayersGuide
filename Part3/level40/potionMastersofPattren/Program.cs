c549122d4df1f69e8da0c67b543c859f130eb23d
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-07-14 14:33:08 +0800

    Fix choice mapping.
    Use pattern matching to create new potion.

--- Part3/level40/potionMastersofPattren/Program.cs
+++ Part3/level40/potionMastersofPattren/Program.cs
@@ -18,15 +18,44 @@ while (choice != ConsoleKey.C)
 {
     var nextIngredient = choice switch
     {
-        ConsoleKey.D1 or ConsoleKey.NumPad1 => Ingredient.Stardust,
-        ConsoleKey.D2 or ConsoleKey.NumPad2 => Ingredient.SnakeVenom,
-        ConsoleKey.D3 or ConsoleKey.NumPad3 => Ingredient.DragonBreath,
-        ConsoleKey.D4 or ConsoleKey.NumPad4 => Ingredient.ShadowGlass,
-        ConsoleKey.D5 or ConsoleKey.NumPad5 => Ingredient.EyeShine,
+        ConsoleKey.D0 or ConsoleKey.NumPad0 => Ingredient.DragonBreath,
+        ConsoleKey.D1 or ConsoleKey.NumPad1 => Ingredient.EyeShine,
+        ConsoleKey.D2 or ConsoleKey.NumPad2 => Ingredient.ShadowGlass,
+        ConsoleKey.D3 or ConsoleKey.NumPad3 => Ingredient.SnakeVenom,
+        ConsoleKey.D4 or ConsoleKey.NumPad4 => Ingredient.Stardust,
         _                                   => Ingredient.Water
     };
 
     Console.WriteLine($"\nYou add {nextIngredient.Humanize()} to your vial of {potion.Humanize()}.");
+
+    potion = (potion, nextIngredient) switch
+    {
+        (Potion.Water, Ingredient.Stardust)               => Potion.Elixir,
+        (Potion.Elixir, Ingredient.SnakeVenom)            => Potion.PoisionPotion,
+        (Potion.Elixir, Ingredient.DragonBreath)          => Potion.FlyingPotion,
+        (Potion.Elixir, Ingredient.ShadowGlass)           => Potion.InvisibilityPotion,
+        (Potion.Elixir, Ingredient.EyeShine)              => Potion.NightSightPotion,
+        (Potion.NightSightPotion, Ingredient.ShadowGlass) => Potion.CloudyBrew,
+        (Potion.InvisibilityPotion, Ingredient.EyeShine)  => Potion.CloudyBrew,
+        (Potion.CloudyBrew, Ingredient.Stardust)          => Potion.WraithPotion,
+        _                                                 => Potion.Ruined
+    };
+    var message = potion switch
+    {
+        Potion.CloudyBrew         => "turns a cloudy grey",
+        Potion.Elixir             => "becomes a milky white",
+        Potion.FlyingPotion       => "shines like gold",
+        Potion.InvisibilityPotion => "flashes brightly, then becomes clear once again",
+        Potion.NightSightPotion   => "slowly thickens and becomes a silvery colour",
+        Potion.PoisionPotion      => "emits a sweet smell and becomes a lurid green",
+        Potion.WraithPotion       => "becomes as black as night. You see a ghostly image appear above the vial",
+        _                         => "explodes with a loud bang!"
+    };
+    var result = potion == Potion.Ruined
+                     ? "Your potion has been ruined. Sighing, you reach for another vial of water, and try again."
+                     : $"You have created a {potion.Humanize()}!";
+    Console.WriteLine($"The potion {message}. {result}");
+    potion = potion == Potion.Ruined ? Potion.Water : potion;
     choice = Console.ReadKey().Key;
 }
 
