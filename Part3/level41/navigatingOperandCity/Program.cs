6c1cfd1df1fd2521c7e2de6f608332146f26882c
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-09-25 18:46:05 +0800

    Add + operator for BlockCoordinate.

--- Part3/level41/navigatingOperandCity/Program.cs
+++ Part3/level41/navigatingOperandCity/Program.cs
@@ -3,7 +3,19 @@
 /// <summary>
 ///     The Block's location
 /// </summary>
-public record BlockCoordinate(int Row, int Column);
+public record BlockCoordinate(int Row, int Column)
+{
+    /// <summary>
+    ///     Implements the operator +.
+    /// </summary>
+    /// <param name="coordinate">The coordinate.</param>
+    /// <param name="offset">The offset.</param>
+    /// <returns>
+    ///     A <c>BlockCoordinate</c> with the coordinates matching the original coordinates + the offset.
+    /// </returns>
+    public static BlockCoordinate operator +(BlockCoordinate coordinate, BlockOffset offset) =>
+        new (coordinate.Row + offset.RowOffset, coordinate.Column + offset.ColumnOffset);
+}
 
 /// <summary>
 ///     The relative distance between two blocks
