468b709c0878375e251d08a47d886bd225579412
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-10-08 15:30:43 +0800

    Add RecentNumbers class, generate numbers in infinite loop.

---
+++ Part3/level43/repeatingStream/Program.cs
@@ -0,0 +1,21 @@
+﻿// Example program demonstrating the use of multi-threading.
+
+#region Using Directives
+using repeatingStream;
+#endregion
+
+var rnd               = new Random();
+var mostRecentNumbers = new RecentNumbers();
+
+while (true)
+{
+    GenerateNumber();
+}
+
+void GenerateNumber()
+{
+    var number = rnd.Next(0, 10); // 0 - 9
+    mostRecentNumbers.AddNumber(number);
+    Console.WriteLine(number);
+    Thread.Sleep(1000); // Sleep one second.
+}
---
+++ Part3/level43/repeatingStream/RecentNumbers.cs
@@ -0,0 +1,21 @@
+﻿// repeatingStream
+
+namespace repeatingStream;
+
+public class RecentNumbers
+{
+    #region Fields
+    public Queue<int> Numbers = new ();
+    #endregion
+
+    public void AddNumber(int number)
+    {
+        if (Numbers.Count == 2)
+        {
+            // Remove the first number, keep the most recent.
+            Numbers.Dequeue();
+        }
+
+        Numbers.Enqueue(number);
+    }
+}
