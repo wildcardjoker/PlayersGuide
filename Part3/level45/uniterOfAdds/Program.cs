d01f4533288aa7492247becb5962f715807855bc
wildcardjoker <wildcardjoker@users.noreply.github.com>
2023-11-04 16:52:30 +0800

    Uniter of Adds. Add sample for dynamic typing.

---
+++ Part3/level45/uniterOfAdds/Program.cs
@@ -0,0 +1,9 @@
+﻿// Use Add() method with dynamic typing to add two values together
+
+Console.WriteLine(Add(1,            1));
+Console.WriteLine(Add(25.5,         25.5));
+Console.WriteLine(Add("Hello",      " World"));
+Console.WriteLine(Add(DateTime.Now, new TimeSpan(1, 0, 0)));
+return;
+
+static dynamic Add(dynamic a, dynamic b) => a + b;
