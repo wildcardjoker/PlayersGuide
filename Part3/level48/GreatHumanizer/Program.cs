8b4652d0f1f7e1ac3ccd9cc38ae60c45f61740de
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-01-01 17:06:43 +0800

    Add Humanizer package.

--- Part3/level48/GreatHumanizer/Program.cs
+++ Part3/level48/GreatHumanizer/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(30)}");
+﻿#region Using Directives
+using Humanizer;
+#endregion
+
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(30)}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(30).Humanize()}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(2.5).Humanize()}");
+Console.WriteLine($"When is the feast? {DateTime.UtcNow.AddHours(50).Humanize()}");
