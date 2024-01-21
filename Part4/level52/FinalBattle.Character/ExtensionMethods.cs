d7a4ed9f85a1c7b125bfa71c9bbea3499feec90b
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-01-21 19:35:41 +0800

    Make targetCharacter non-nullable.

--- Part4/level52/FinalBattle.Character/ExtensionMethods.cs
+++ Part4/level52/FinalBattle.Character/ExtensionMethods.cs
@@ -19,18 +19,17 @@ public static class ExtensionMethods
     /// <exception cref="ArgumentOutOfRangeException">The specified action is not valid.</exception>
     public static AttackData PerformAction(this Character character, Action action, Character? targetCharacter)
     {
-        var characterNameUpperCase = character.Name;
         return action switch
         {
-            Action.Nothing => new AttackData($"{characterNameUpperCase} did NOTHING."),
-            Action.Attack  => AttackTarget(character, character.Attacks.First(), targetCharacter),
+            Action.Nothing => new AttackData($"{character} did NOTHING."),
+            Action.Attack  => AttackTarget(character, character.Attacks.First(), targetCharacter!),
             _              => throw new ArgumentOutOfRangeException(nameof(action), action, null)
         };
     }
 
-    private static AttackData AttackTarget(Character character, Attack attack, Character? targetCharacter)
+    private static AttackData AttackTarget(Character character, Attack attack, Character targetCharacter)
     {
-        var attackDescription = $"{character.Name} used {attack.Name} on {targetCharacter?.Name ?? "UNKNOWN TARGET"}.";
-        return new AttackData(attackDescription, attack.CalculateDamage());
+        var attackDescription = $"{character} used {attack} on {targetCharacter}.";
+        return new AttackData(attack, attackDescription, attack.CalculateDamage());
     }
 }
