4609adb2a9c663b6af6a611de851d5ada656d1a2
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-01-21 12:56:55 +0800

    First boss battle: Building Character
    - Add a `Character` library.
    - Create two parties of one Character per party
    - Each Character in each party performs an action
    - Action is current hard-coded to `nothing`
    - Display character's turn and action to console
    - Repeat indefinitely

---
+++ part4/level52/Character/Character.cs
@@ -0,0 +1,46 @@
+﻿namespace FinalBattle.Character
+{
+    /// <summary>
+    ///     Represents a Character
+    /// </summary>
+
+    // TODO: Add Interface and common classes
+    public class Character
+    {
+        #region Constructors
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Character" /> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="hitPoints">The hit points.</param>
+        public Character(string name, int hitPoints)
+        {
+            Name      = name;
+            HitPoints = hitPoints;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        ///     Gets the hit points.
+        /// </summary>
+        /// <value>
+        ///     The hit points.
+        /// </value>
+        public int HitPoints {get; private set;}
+
+        /// <summary>
+        ///     Gets the name.
+        /// </summary>
+        /// <value>
+        ///     The name.
+        /// </value>
+        public string Name {get;}
+
+        /// <summary>
+        ///     Convert the character's name to Uppercase.
+        /// </summary>
+        public string UpperName => Name.ToUpper();
+        #endregion
+    }
+}
