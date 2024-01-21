c13c732f51c326c34a4ad91578de310705472fbe
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-01-21 19:35:06 +0800

    Add Attack property, default constructor.

--- Part4/level52/FinalBattle.Character/AttackData.cs
+++ Part4/level52/FinalBattle.Character/AttackData.cs
@@ -12,15 +12,36 @@ public class AttackData
     ///     Initializes a new instance of the <see cref="AttackData" /> class.
     /// </summary>
     /// <param name="description">The description.</param>
-    /// <param name="damage">The damage.</param>
-    public AttackData(string description, int damage = 0)
+    public AttackData(string description)
     {
+        Attack      = null;
+        Description = description;
+        Damage      = 0;
+    }
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="AttackData" /> class.
+    /// </summary>
+    /// <param name="attack">The attack.</param>
+    /// <param name="description">The description.</param>
+    /// <param name="damage">The damage.</param>
+    public AttackData(Attack attack, string description, int damage = 0)
+    {
+        Attack      = attack;
         Damage      = damage;
         Description = description;
     }
     #endregion
 
     #region Properties
+    /// <summary>
+    ///     Gets the attack.
+    /// </summary>
+    /// <value>
+    ///     The attack.
+    /// </value>
+    public Attack? Attack {get; private set;}
+
     /// <summary>
     ///     Gets the damage.
     /// </summary>
@@ -29,6 +50,12 @@ public class AttackData
     /// </value>
     public int Damage {get; private set;}
 
+    /// <summary>
+    ///     Gets the description.
+    /// </summary>
+    /// <value>
+    ///     The description.
+    /// </value>
     public string Description {get; private set;}
     #endregion
 }
