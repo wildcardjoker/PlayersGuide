58efe20908685be243518ef6e7af1502583d1e98
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-01-21 19:01:09 +0800

    Update Attack class. Make class abstract. Add Damage variable. Add CalculateDamage() method.

--- Part4/level52/FinalBattle.Character/Attack.cs
+++ Part4/level52/FinalBattle.Character/Attack.cs
@@ -5,17 +5,25 @@ namespace FinalBattle.Character;
 /// <summary>
 ///     An object representing an attack on another character.
 /// </summary>
-public class Attack
+public abstract class Attack
 {
     #region Constructors
     /// <summary>
     ///     Initializes a new instance of the <see cref="Attack" /> class.
     /// </summary>
     /// <param name="name">The name.</param>
-    public Attack(string name) => Name = name;
+    protected Attack(string name) => Name = name;
     #endregion
 
     #region Properties
+    /// <summary>
+    ///     Gets the damage.
+    /// </summary>
+    /// <value>
+    ///     The damage.
+    /// </value>
+    public int Damage {get; set;}
+
     /// <summary>
     ///     Gets the name.
     /// </summary>
@@ -24,4 +32,10 @@ public class Attack
     /// </value>
     public string Name {get; private set;}
     #endregion
+
+    /// <summary>
+    ///     Calculates the damage.
+    /// </summary>
+    /// <returns>The amount of damage inflicted by the character.</returns>
+    public abstract int CalculateDamage();
 }
