71dccf4bf56b0c8798576e4dda9610ab74caf7a6
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-02-25 17:49:05 +0800

    Add Human Player. Allow player to choose between Human/Human, Human/Computer, and Computer/Computer game modes.

---
+++ Part4/level52/FinalBattle.Character/Player/HumanPlayer.cs
@@ -0,0 +1,33 @@
+﻿// FinalBattle.Character
+
+namespace FinalBattle.Character.Player;
+
+#region Using Directives
+using Humanizer;
+#endregion
+
+public class HumanPlayer : IPlayer
+{
+    #region IPlayer Members
+    #region Implementation of IPlayer
+    /// <inheritdoc />
+    public Action SelectAction()
+    {
+        var actionValues = (int[]) Enum.GetValues(typeof(Action));
+        foreach (Action value in actionValues)
+        {
+            Console.WriteLine($"{(int) value} - {value.Humanize()}");
+        }
+
+        var action = 0;
+        while (!actionValues.Contains(action))
+        {
+            Console.Write("What do you want to do? ");
+            int.TryParse(Console.ReadLine(), out action);
+        }
+
+        return (Action) action;
+    }
+    #endregion
+    #endregion
+}
