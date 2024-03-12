e6d3be66d0aedf39d0b92654692d238f397e97bf
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-03-12 20:52:21 +0800

    Add base Item class and derived Health Potion.

---
+++ Part4/level52/FinalBattle.Character/Items/Item.cs
@@ -0,0 +1,27 @@
+﻿namespace FinalBattle.Character.Items
+{
+    /// <summary>
+    ///     Base class for items.
+    /// </summary>
+    public abstract class Item
+    {
+        #region Constructors
+        protected Item(string name) => Name = name.ToUpper();
+        #endregion
+
+        #region Properties
+        /// <summary>
+        ///     Gets or sets the name of the item.
+        /// </summary>
+        /// <value>
+        ///     The name.
+        /// </value>
+        protected string Name {get; set;}
+        #endregion
+
+        /// <summary>
+        ///     Uses this instance.
+        /// </summary>
+        public abstract void Use(Character character);
+    }
+}
