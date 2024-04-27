495a97bbb256ae423e6826251dd736d5d7b441be
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-04-27 12:23:12 +0800

    Make player the base object for the game. This simplifies item use etc.

--- Part4/level52/FinalBattle.Character/Player/ComputerPlayer.cs
+++ Part4/level52/FinalBattle.Character/Player/ComputerPlayer.cs
@@ -4,19 +4,24 @@
     ///     A computer/AI player. All decisions are made by the computer.
     /// </summary>
     /// <seealso cref="IPlayer" />
-    public class ComputerPlayer : IPlayer
+    public class ComputerPlayer : Player
     {
         #region Fields
         private readonly Random _random = new Random();
         #endregion
 
-        #region IPlayer Members
+        #region Constructors
         /// <inheritdoc />
-        public Action SelectAction(Character character, Inventory inventory)
+        public ComputerPlayer() {}
+        #endregion
+
+        /// <inheritdoc />
+        public override Action SelectAction()
         {
+            var currentCharacter = CurrentParty.Characters.First(x => x.IsActive);
             Thread.Sleep(500); // simulate decision
             var availableActions = Enum.GetNames(typeof(Action)).ToList();
-            if (inventory.ContainsHealthPotion && character.IsWounded && _random.Next(1, 101) <= 25)
+            if (CurrentParty.PartyInventory.ContainsHealthPotion && currentCharacter.IsWounded && _random.Next(1, 101) <= 25)
             {
                 return Action.UseItem;
             }
@@ -27,7 +32,14 @@
         }
 
         /// <inheritdoc />
-        public int SelectTarget(List<Character> characters) => _random.Next(characters.Count);
-        #endregion
+        public override int SelectTarget(List<Character> characters) => _random.Next(characters.Count);
+
+        /// <inheritdoc />
+        public override void UseItem(int index)
+        {
+            // Only Health potions at this stage.
+            index = 0;
+            CurrentParty.PartyInventory.UseItem(CurrentCharacter, index);
+        }
     }
 }
