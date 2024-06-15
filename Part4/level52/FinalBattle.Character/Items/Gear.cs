1e077fbf5cd7937c822e0d1f876b875d2f896821
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-06-15 11:33:08 +0800

    Add Gear inventory item.

---
+++ Part4/level52/FinalBattle.Character/Items/Gear.cs
@@ -0,0 +1,28 @@
+﻿namespace FinalBattle.Character.Items
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     Base class for items.
+    /// </summary>
+    public abstract class Gear : InventoryItem
+    {
+        #region Constructors
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="Gear" /> class.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        protected Gear(string name) => Name = name.ToUpper();
+        #endregion
+
+        /// <summary>
+        ///     Attacks the specified character.
+        /// </summary>
+        /// <param name="character">The character.</param>
+        public abstract void Attack(Character character);
+
+        /// <summary>
+        ///     Uses this instance.
+        /// </summary>
+        public abstract void Equip(Character character);
+    }
+}
