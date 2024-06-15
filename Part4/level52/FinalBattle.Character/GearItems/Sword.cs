31cd9636d3ddeb0f9b83f9e6539680c63c396215
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-06-15 12:10:05 +0800

    Add a Sword.

---
+++ Part4/level52/FinalBattle.Character/GearItems/Sword.cs
@@ -0,0 +1,10 @@
+﻿namespace FinalBattle.Character.GearItems
+{
+    internal class Sword : Gear
+    {
+        #region Constructors
+        /// <inheritdoc />
+        public Sword() : base("Sword", "slash", 2) {}
+        #endregion
+    }
+}
