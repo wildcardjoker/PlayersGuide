dd9ef5274bebbe86223e640e4ab0846a3abc2d60
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-07-07 12:58:29 +0800

    Use specified Description or class name if null.

--- Part4/level52/FinalBattle.Character/GearItems/Gear.cs
+++ Part4/level52/FinalBattle.Character/GearItems/Gear.cs
@@ -9,9 +9,16 @@
     /// </summary>
     public abstract class Gear : Attack
     {
+        #region Fields
+        private string? _description;
+        #endregion
+
         #region Constructors
         /// <inheritdoc />
         protected Gear(string name, float chanceToHit, int damage) : base(name, chanceToHit, damage) {}
+
+        /// <inheritdoc />
+        protected Gear(string name, string description, float chanceToHit, int damage) : base(description, chanceToHit, damage) => Description = name;
         #endregion
 
         #region Overrides of Object
@@ -22,7 +29,11 @@
         ///     The gear's description (weapon type)
         /// </summary>
         /// <returns></returns>
-        public string Description => GetType().Name.ToUpper();
+        public string Description
+        {
+            get => string.IsNullOrWhiteSpace(_description) ? GetType().Name.ToUpper() : _description.ToUpper();
+            private set => _description = value;
+        }
         #endregion
     }
 }
