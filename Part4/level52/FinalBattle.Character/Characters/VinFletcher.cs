25914363199cdf189901405a16ecf4c69b1de491
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-07-07 12:59:42 +0800

    Add Vin Fletcher and Vins Bow.

---
+++ Part4/level52/FinalBattle.Character/Characters/VinFletcher.cs
@@ -0,0 +1,19 @@
+﻿// FinalBattle.Character
+
+namespace FinalBattle.Character.Characters;
+
+#region Using Directives
+using Attacks;
+using GearItems;
+#endregion
+
+/// <summary>
+///     The True Programmer
+/// </summary>
+public class VinFletcher : Character
+{
+    #region Constructors
+    /// <inheritdoc />
+    public VinFletcher() : base("Vin Fletcher", new[] {new Punch()}, 15) => EquippedGear = new VinsBow();
+    #endregion
+}
---
+++ Part4/level52/FinalBattle.Character/GearItems/VinsBow.cs
@@ -0,0 +1,23 @@
+﻿namespace FinalBattle.Character.GearItems
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     Vin's Bow. Has a quick shot attack.
+    /// </summary>
+    /// <remarks>
+    ///     The attack will be successful 50% of the time, and deal 3 points of damage.
+    /// </remarks>
+    /// <seealso cref="T:FinalBattle.Character.GearItems.Gear" />
+    public class VinsBow : Gear
+    {
+        #region Constructors
+        /// <inheritdoc />
+        public VinsBow() : base("Vin's Bow", "Quick Shot", 0.5f, 3) {}
+        #endregion
+
+        #region Overrides of Attack
+        /// <inheritdoc />
+        public override int CalculateDamage() => Damage;
+        #endregion
+    }
+}
