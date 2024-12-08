d2f215f990da5df97914f599154261371f002167
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-12-08 11:48:11 +0800

    Update AttackModifier and add NoAttack class
    * Added `HasModifier` property to `AttackModifier`.
    * Changed `Modifier` and `Name` properties to private in `AttackModifier`.
    * Added `ModifyAttack` method to `AttackModifier`.
    * Overridden `ToString` method in `AttackModifier` to return the name in uppercase.
    * Added `NoAttack` class in `FinalBattle.Character.Attacks` namespace with default zero damage and `NotImplementedException` for `CalculateDamage`.

--- Part4/level52/FinalBattle.Character/AttackModifier.cs
+++ Part4/level52/FinalBattle.Character/AttackModifier.cs
@@ -2,6 +2,10 @@
 
 namespace FinalBattle.Character;
 
+#region Using Directives
+using Attacks;
+#endregion
+
 /// <summary>
 ///     Represents a modifier that can be applied to an attack.
 /// </summary>
@@ -22,13 +26,36 @@ public class AttackModifier
 
     #region Properties
     /// <summary>
+    ///     Gets a value indicating whether this instance has an Attack Modifier.
+    /// </summary>
+    /// <value>
+    ///     <c>true</c> if this instance has an Attack Modifier; otherwise, <c>false</c>.
+    /// </value>
+    public bool HasModifier => Modifier != 0;
+
+    /// <summary>
     ///     Gets the value of the attack modifier.
     /// </summary>
-    public int Modifier {get;}
+    private int Modifier {get;}
 
     /// <summary>
     ///     Gets the name of the attack modifier.
     /// </summary>
-    public string Name {get;}
+    private string Name {get;}
+    #endregion
+
+    /// <summary>
+    ///     Modifies the attack by Modifier.
+    /// </summary>
+    /// <param name="attackData">The original attack data.</param>
+    /// <returns>A new <c>AttackData</c> object with a modified damage amount.</returns>
+    public AttackData ModifyAttack(AttackData attackData) => new (
+        attackData.Attack ?? new NoAttack(),
+        $"{this} reduced damage by {Math.Abs(Modifier)}",
+        attackData.Damage + Modifier);
+
+    #region Overrides of Object
+    /// <inheritdoc />
+    public override string ToString() => Name.ToUpper();
     #endregion
 }
---
+++ Part4/level52/FinalBattle.Character/Attacks/NoAttack.cs
@@ -0,0 +1,17 @@
+﻿// FinalBattle.Character
+
+namespace FinalBattle.Character.Attacks;
+
+/// <inheritdoc />
+public class NoAttack : Attack
+{
+    #region Constructors
+    /// <inheritdoc />
+    public NoAttack() : base(nameof(NoAttack), 0, 0) {}
+    #endregion
+
+    #region Overrides of Attack
+    /// <inheritdoc />
+    public override int CalculateDamage() => throw new NotImplementedException();
+    #endregion
+}
