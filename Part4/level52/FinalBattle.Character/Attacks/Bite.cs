679527a03b1f1af9fe6e4f6b5bc89fdaa292fe16
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-12-08 10:30:44 +0800

    Add Stone Amarok character and Bite attack classes
    * Introduce `Bite` class in `FinalBattle.Character.Attacks` namespace.
      * Inherits from `Attack` class.
      * Constructor initializes name and damage values.
      * Overrides `CalculateDamage` to return fixed damage of 1.
    * Add `StoneAmarok` class in `FinalBattle.Character.Characters` namespace.
      * Represents a Stone Amarok character with 4 HP.
      * Inherits from `Character` class.
      * Initializes with a name, an array of attacks (including `Bite`), and HP.

---
+++ Part4/level52/FinalBattle.Character/Attacks/Bite.cs
@@ -0,0 +1,21 @@
+﻿// FinalBattle.Character
+
+namespace FinalBattle.Character.Attacks;
+
+/// <inheritdoc />
+/// <summary>
+///     Represents a Bite attack
+/// </summary>
+/// <seealso cref="T:FinalBattle.Character.Attack" />
+public class Bite : Attack
+{
+    #region Constructors
+    /// <inheritdoc />
+    public Bite() : base(nameof(Bite), 1, 1) {}
+    #endregion
+
+    #region Overrides of Attack
+    /// <inheritdoc />
+    public override int CalculateDamage() => 1;
+    #endregion
+}
---
+++ Part4/level52/FinalBattle.Character/Characters/StoneAmarok.cs
@@ -0,0 +1,18 @@
+﻿namespace FinalBattle.Character.Characters
+{
+    #region Using Directives
+    using Attacks;
+    #endregion
+
+    /// <summary>
+    ///     A Stone Amarok. Has 4 HP and a Bite attack
+    /// </summary>
+    /// <seealso cref="FinalBattle.Character.Character" />
+    public class StoneAmarok : Character
+    {
+        #region Constructors
+        /// <inheritdoc />
+        public StoneAmarok() : base("Stone Amarok", new[] {new Bite()}, 4) {}
+        #endregion
+    }
+}
