f859f4025eaf56faf2dbf9492f65195dc39471bb
wildcardjoker <wildcardjoker@users.noreply.github.com>
2024-12-22 10:36:15 +0800

    Add CannonShot attack and MylaraAndSkorin character team
    * Introduced `FinalBattle.Character.Attacks` namespace.
    * Added `CannonShot` class inheriting from `Attack` with `CalculateDamage` method.
    * Introduced `FinalBattle.Character.Characters` namespace.
    * Added `MylaraAndSkorin` class with `CannonShot` attack, `AttackModifier`, and 15 health.

---
+++ Part4/level52/FinalBattle.Character/Attacks/Cannon.cs
@@ -0,0 +1,29 @@
+﻿namespace FinalBattle.Character.Attacks
+{
+    /// <inheritdoc />
+    /// <summary>
+    ///     A Punch attack
+    /// </summary>
+    /// <seealso cref="T:FinalBattle.Character.Attack" />
+    internal class CannonShot : Attack
+    {
+        #region Fields
+        private int _rounds;
+        #endregion
+
+        #region Constructors
+        /// <inheritdoc />
+        public CannonShot() : base("Cannon of Consolas", 1, 1) {}
+        #endregion
+
+        #region Implementation of IAttack
+        /// <inheritdoc />
+        public override int CalculateDamage()
+        {
+            _rounds++;
+            Console.WriteLine($"Rounds: {_rounds}");
+            return _rounds % 15 == 0 ? 5 : _rounds % 5 == 0 || _rounds % 3 == 0 ? 2 : 1;
+        }
+        #endregion
+    }
+}
---
+++ Part4/level52/FinalBattle.Character/Characters/MylaraAndSkorin.cs
@@ -0,0 +1,20 @@
+﻿// FinalBattle.Character
+
+namespace FinalBattle.Character.Characters;
+
+#region Using Directives
+using Attacks;
+#endregion
+
+/// <inheritdoc />
+/// <summary>
+///     Mylara and Skorin: a team who fire a cannon shot at their enemies.
+/// </summary>
+/// <seealso cref="T:FinalBattle.Character.Character" />
+public class MylaraAndSkorin : Character
+{
+    #region Constructors
+    /// <inheritdoc />
+    public MylaraAndSkorin() : base("Mylara and Skorin", new[] {new CannonShot()}, new AttackModifier(), 15) {}
+    #endregion
+}
