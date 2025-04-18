f0847dd3fd641fa011a67b140a00c02c31afa815
wildcardjoker <wildcardjoker@users.noreply.github.com>
2025-04-18 11:15:16 +0800

    Equip the True Programmer with a `Flaming Sword`

--- Part4/level52/FinalBattle.Character/Characters/TrueProgrammer.cs
+++ Part4/level52/FinalBattle.Character/Characters/TrueProgrammer.cs
@@ -15,6 +15,7 @@ public class TrueProgrammer : Character
 {
     #region Constructors
     /// <inheritdoc />
-    public TrueProgrammer(string name) : base(name, new[] {new Punch()}, new AttackModifier("Object Sight", DamageType.Decoding, -2), 25) => EquippedGear = new Sword();
+    public TrueProgrammer(string name) : base(name, new[] {new Punch()}, new AttackModifier("Object Sight", AttackModifierType.Defensive, DamageType.Decoding, -2), 25) =>
+        EquippedGear = new FlamingSword();
     #endregion
 }
