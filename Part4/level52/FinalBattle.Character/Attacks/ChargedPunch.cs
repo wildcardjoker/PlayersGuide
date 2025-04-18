07e7a654f7a9d5eae14cc8e85ddb0954ab5af03b
wildcardjoker <wildcardjoker@users.noreply.github.com>
2025-04-18 11:50:06 +0800

    Add ChargedPunch class and update TrueProgrammer
    - Introduced a new `ChargedPunch` class in the `FinalBattle.Character.Attacks` namespace.
    - Updated the `TrueProgrammer` class constructor to include `ChargedPunch` alongside `Punch`.

---
+++ Part4/level52/FinalBattle.Character/Attacks/ChargedPunch.cs
@@ -0,0 +1,19 @@
+﻿namespace FinalBattle.Character.Attacks
+{
+    /// <summary>
+    ///     A Punch attack
+    /// </summary>
+    /// <seealso cref="FinalBattle.Character.Attack" />
+    internal class ChargedPunch : Attack
+    {
+        #region Constructors
+        /// <inheritdoc />
+        public ChargedPunch() : base("charged punch", DamageType.Physical, .9f, 2, new AttackModifier("Charge", AttackModifierType.Offensive, DamageType.Physical, 1)) {}
+        #endregion
+
+        #region Implementation of IAttack
+        /// <inheritdoc />
+        public override int CalculateDamage() => Damage;
+        #endregion
+    }
+}
--- Part4/level52/FinalBattle.Character/Characters/TrueProgrammer.cs
+++ Part4/level52/FinalBattle.Character/Characters/TrueProgrammer.cs
@@ -15,7 +15,11 @@ public class TrueProgrammer : Character
 {
     #region Constructors
     /// <inheritdoc />
-    public TrueProgrammer(string name) : base(name, new[] {new Punch()}, new AttackModifier("Object Sight", AttackModifierType.Defensive, DamageType.Decoding, -2), 25) =>
+    public TrueProgrammer(string name) : base(
+        name,
+        new Attack[] {new Punch(), new ChargedPunch()},
+        new AttackModifier("Object Sight", AttackModifierType.Defensive, DamageType.Decoding, -2),
+        25) =>
         EquippedGear = new FlamingSword();
     #endregion
 }
