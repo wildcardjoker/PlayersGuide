a3cf091ea1d08f6b724ad1d2970ea9f786880909
wildcardjoker <wildcardjoker@users.noreply.github.com>
2025-04-27 12:09:36 +0800

    Update Unravel class namespace and constructor parameters
    - Changed namespace to `FinalBattle.Character.Attacks`.
    - Added XML documentation for the `Attack` class reference.
    - Updated constructor: damage multiplier from `0.8f` to `0.95f` and base damage from `4` to `10`.

--- Part4/level52/FinalBattle.Character/Attacks/Unravel.cs
+++ Part4/level52/FinalBattle.Character/Attacks/Unravel.cs
@@ -7,6 +7,7 @@ namespace FinalBattle.Character.Attacks;
 ///     An unravelling attack
 /// </summary>
 /// <seealso cref="T:FinalBattle.Character.Attack" />
+/// <seealso cref="T:FinalBattle.Character.Attack" />
 public class Unravel : Attack
 {
     #region Fields
@@ -15,7 +16,7 @@ public class Unravel : Attack
 
     #region Constructors
     /// <inheritdoc />
-    public Unravel() : base("Unravelling", DamageType.Decoding, 0.8f, 4) {}
+    public Unravel() : base("Unravelling", DamageType.Decoding, 0.95f, 10) {}
     #endregion
 
     #region Implementation of IAttack
